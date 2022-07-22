f6df344228d46a7b77487c80d40cae0ce0965d1c
quocpa <quocpa@vng.com.vn>
2022-07-22 12:27:47 +0700

    add filter=stream to get param

--- dev/script.js
+++ dev/script.js
@@ -172,7 +172,7 @@ function goFetchComment(afterNode = ""){
     let limit = $("#limit").val();
     SessionData.currentXhr = $.ajax({
         method: "GET",
-        url: `https://graph.facebook.com/v13.0/${pageId}_${postId}/comments?access_token=${accessToken}&limit=${limit}&fields=message,id${afterParam}`,
+        url: `https://graph.facebook.com/v13.0/${pageId}_${postId}/comments?access_token=${accessToken}&limit=${limit}&filter=stream&fields=message,id${afterParam}`,
         success: onFetchComment,
         error: (e)=>{onError(e, "Không lấy được comment");}
     })
